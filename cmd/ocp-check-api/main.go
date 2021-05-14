ad830684df5cb51937d01d8e5cf4fae09d25c435
vcherdantsev <v.cherdantsev@ngs.ru>
2021-05-15 00:53:28 +0700

    Printing emoji

---
+++ cmd/ocp-check-api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/enescakir/emoji"
+)
+
+func main() {
+	fmt.Printf("Hello from %v\n", emoji.Package)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ozoncp/ocp-check-api
 
 go 1.16
+
+require github.com/enescakir/emoji v1.0.0 // indirect
