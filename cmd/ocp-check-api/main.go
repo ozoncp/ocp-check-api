1a2baa0cb55eb3bf216a555e021d9f783ae4f28d
vcherdantsev <v.cherdantsev@ngs.ru>
2021-06-10 01:24:43 +0700

    Task 6: gRPC CRUD service implementaion with zerolog.

--- cmd/ocp-check-api/main.go
+++ cmd/ocp-check-api/main.go
@@ -1,61 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
+	"net"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/ozoncp/ocp-check-api/internal/flusher"
-	"github.com/ozoncp/ocp-check-api/internal/models"
-	"github.com/ozoncp/ocp-check-api/internal/repo"
-	"github.com/ozoncp/ocp-check-api/internal/saver"
+	api "github.com/ozoncp/ocp-check-api/internal/api"
+	desc "github.com/ozoncp/ocp-check-api/pkg/ocp-check-api"
+	grpczerolog "github.com/philip-bui/grpc-zerolog"
+	zerolog "github.com/rs/zerolog"
+	"google.golang.org/grpc"
+)
+
+const (
+	grpcAddress = "0.0.0.0:8083"
 )
 
 func Greeting(name string) string {
 	return fmt.Sprintf("Hello, %v!", name)
 }
 
-// Чтение консольного ввода
-func read(r io.Reader) <-chan string {
-	consoleCh := make(chan string)
-	go func() {
-		defer close(consoleCh)
-		scan := bufio.NewScanner(r)
-		for scan.Scan() {
-			s := scan.Text()
-			consoleCh <- s
-		}
-	}()
-	return consoleCh
+func runGrpcServer(address string) error {
+	// console Zerolog instance.
+	log := zerolog.New(os.Stdout)
+
+	listen, err := net.Listen("tcp4", address)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to listen")
+	}
+
+	s := grpc.NewServer(grpczerolog.UnaryInterceptorWithLogger(&log))
+
+	desc.RegisterOcpCheckApiServer(s, api.NewOcpCheckApi())
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatal().Err(err).Msg("failed to serve")
+	}
+
+	return nil
 }
 
 func main() {
-	termChan := make(chan os.Signal, 1)
-	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
-
-	capacity := uint(10)
-	chunkSize := 100
-	repo := repo.NewCheckRepo()
-	flusher := flusher.NewCheckFlusher(chunkSize, repo)
-
-	saver := saver.NewSaver(capacity, time.Second*3, flusher)
-	if saver != nil {
-		saver.Init()
-	}
-
-	defer saver.Close()
-	consoleCh := read(os.Stdin)
-
-	go func() {
-		fmt.Println("Enter new string to save models.Check or press Ctrl+C to exit...")
-		for msg := range consoleCh {
-			_ = saver.Save(models.Check{ID: uint64(len(msg)), Success: len(msg) > 2})
-		}
-	}()
-
-	<-termChan // Ctrl+C pressed!
+	_ = runGrpcServer(grpcAddress)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module github.com/ozoncp/ocp-check-api
 go 1.16
 
 require (
+	github.com/envoyproxy/protoc-gen-validate v0.1.0
 	github.com/golang/mock v1.5.0
+	github.com/golang/protobuf v1.5.2
 	github.com/google/go-cmp v0.5.5
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/onsi/ginkgo v1.16.2
 	github.com/onsi/gomega v1.13.0
+	github.com/philip-bui/grpc-zerolog v1.0.1 // indirect
+	github.com/rs/zerolog v1.22.0 // indirect
 	github.com/stretchr/testify v1.7.0
+	google.golang.org/genproto v0.0.0-20210608205507-b6d2f5bf0d7d
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
 )
